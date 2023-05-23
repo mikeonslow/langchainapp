e4b1f4ebde80891c2eb5b7fe6fd576d7addbd362
clarityits <clarityits@clarityvoice.com>
2023-05-23 01:11:08 -0400

    Added linkedin via proxycurl module, added mock data endpoint and live data EP to save proxycurl credits

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,7 +1,8 @@
 from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
-
+from third_parties.linkedin import scrape_linkedin_profile
+import json
 
 information = """
 
@@ -49,27 +50,39 @@ In 2002, PayPal was acquired by eBay for $1.5 billion in stock, of which Musk—
 
 """
 
+
 def main():
     print("starting application...")
 
-    summary_template = """
-    given the {information} about a person from I want you to create
-    1. a short cummary
-    2. two interesting facts about them
-    """
+    # summary_template = """
+    # given the {information} about a person from I want you to create
+    # 1. a short cummary
+    # 2. two interesting facts about them
+    # """
 
-    summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
+    # summary_prompt_template = PromptTemplate(
+    #     input_variables=["information"], template=summary_template
+    # )
 
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+    # llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 
-    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
-    # with open("/app/output/ice_breaker.txt", "w") as f:
-    #     f.write("Hello LangChain!")
+    # chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+    # # with open("/app/output/ice_breaker.txt", "w") as f:
+    # #     f.write("Hello LangChain!")
+
+    # print(chain.run(information=information))
 
 
-    print(chain.run(information=information))
+linkedin_data = scrape_linkedin_profile(
+    linkedin_profile_url="https://www.linkedin.com/in/mikeonslow/"
+)
+
+# json_data = {'public_identifier': 'mikeonslow', 'profile_pic_url': 'https://s3.us-west-000.backblazeb2.com/proxycurl/person/mikeonslow/profile?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=0004d7f56a0400b0000000001%2F20230523%2Fus-west-000%2Fs3%2Faws4_request&X-Amz-Date=20230523T044708Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=ca52771a3d14aee35cc396a43233c775d8fc6ae7a2b562d4f7d61bf3ab18a2cf', 'first_name': 'Mike', 'last_name': 'Onslow', 'full_name': 'Mike Onslow', 'headline': '\n                  Principal Software Engineer at Clarity Voice\n              ', 'summary': 'I am a full stack software engineer & architect with over 10 years of experience in the field. I love solving problems and have a passion for creating great web applications that make life better for those around me!\n\nCurrently, I work for a premier cloud based Unified  Communications Provider and focus on heading up our software systems division. \n\nI am also very involved in the software engineering community and have a thirst for knowledge. I love to push forth into learning, evaluating and possibly implementing promising technologies to stay ahead of the game.', 'country': 'US', 'city': 'Farmington', 'state': 'Michigan', 'experiences': [{'starts_at': None, 'ends_at': None, 'company': 'Shadow Technologies, L.L.C.', 'company_linkedin_profile_url': None, 'title': 'Senior IT Specialist', 'description': None, 'location': 'Rochester Hills, MI', 'logo_url': None}, {'starts_at': None, 'ends_at': None, 'company': 'Siteworks Web Studios', 'company_linkedin_profile_url': None, 'title': 'Web Application Developer', 'description': None, 'location': 'Ferndale, MI', 'logo_url': None}, {'starts_at': None, 'ends_at': None, 'company': 'Clarity Voice', 'company_linkedin_profile_url': 'https://www.linkedin.com/company/clarity-voice', 'title': 'Principal Software Engineer', 'description': None, 'location': 'Southfield', 'logo_url': 'https://s3.us-west-000.backblazeb2.com/proxycurl/company/clarity-voice/profile?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=0004d7f56a0400b0000000001%2F20230523%2Fus-west-000%2Fs3%2Faws4_request&X-Amz-Date=20230523T044708Z&X-Amz-Expires=1800&X-Amz-SignedHeaders=host&X-Amz-Signature=0cdc492ba4c29377dfb7ece71af0bfe3758bde0d6cd68c2073e4071a36ee4ef3'}, {'starts_at': None, 'ends_at': None, 'company': 'Clarity Voice', 'company_linkedin_profile_url': 'https://www.linkedin.com/company/clarity-voice', 'title': 'Application Developer', 'description': None, 'location': 'Southfield, MI', 'logo_url': 'https://s3.us-west-000.backblazeb2.com/proxycurl/company/clarity-voice/profile?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=0004d7f56a0400b0000000001%2F20230523%2Fus-west-000%2Fs3%2Faws4_request&X-Amz-Date=20230523T044708Z&X-Amz-Expires=1800&X-Amz-SignedHeaders=host&X-Amz-Signature=0cdc492ba4c29377dfb7ece71af0bfe3758bde0d6cd68c2073e4071a36ee4ef3'}, {'starts_at': None, 'ends_at': None, 'company': 'Concise Computer Consulting', 'company_linkedin_profile_url': 'https://www.linkedin.com/company/concise-computer-consulting', 'title': 'Lead IT Specialist', 'description': None, 'location': 'Bloomfield Hills, Michigan', 'logo_url': None}, {'starts_at': None, 'ends_at': None, 'company': 'Hubbell, Roth & Clark', 'company_linkedin_profile_url': 'https://www.linkedin.com/company/hubbell-roth-&-clark', 'title': 'Network Support Analyst', 'description': None, 'location': 'Bloomfield Hills, Michigan', 'logo_url': 'https://s3.us-west-000.backblazeb2.com/proxycurl/company/hubbell-roth-%26-clark/profile?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=0004d7f56a0400b0000000001%2F20230523%2Fus-west-000%2Fs3%2Faws4_request&X-Amz-Date=20230523T044708Z&X-Amz-Expires=1800&X-Amz-SignedHeaders=host&X-Amz-Signature=5fe8f1d6dbee9ac6ab3b379370ded8b00d1186811319adc64aee5fe4f315febf'}, {'starts_at': None, 'ends_at': None, 'company': 'SKF Groupe', 'company_linkedin_profile_url': 'https://www.linkedin.com/company/skf', 'title': 'Computer Systems Analyst', 'description': None, 'location': 'Farmington Hills, MI', 'logo_url': 'https://s3.us-west-000.backblazeb2.com/proxycurl/company/skf/profile?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=0004d7f56a0400b0000000001%2F20230523%2Fus-west-000%2Fs3%2Faws4_request&X-Amz-Date=20230523T044708Z&X-Amz-Expires=1800&X-Amz-SignedHeaders=host&X-Amz-Signature=7740d01dfad58c1cfb0c601fddf88996788353a245b0e0a30508b5701530a4b4'}], 'education': [{'starts_at': None, 'ends_at': None, 'field_of_study': '4.0', 'degree_name': 'Information Systems', 'school': 'Oakland Community College', 'school_linkedin_profile_url': 'https://www.linkedin.com/school/oakland-community-college/', 'description': None, 'logo_url': None, 'grade': None, 'activities_and_societies': None}], 'social_networking_services': [{'service': 'stackoverflow', 'canonical_url': 'stackoverflow.com/users/2188152', 'internal_id': '2188152'}]}
+
+# json_object = json.loads(json_data)
+
+print(linkedin_data)
+
 
 if __name__ == "__main__":
     main()
-
-
---
+++ third_parties/linkedin.py
@@ -0,0 +1,40 @@
+import os
+import requests
+
+mock_data_endpoint = "https://gist.githubusercontent.com/mikeonslow/a95b3ba5bf28fcbfc81c40916be06db6/raw/dd5a41daf1c8842cbcc444c20ae7241ba52ebc6c/mike_onslow_linkedin.json"
+
+
+def scrape_linkedin_profile(linkedin_profile_url: str):
+    """Scrape linkedin profile and return the profile as a dictionary"""
+
+    response = get_data(True)  # MOCK DATA
+    # response = get_data() # Live data
+
+    data = response.json()
+
+    data = {
+        k: v
+        for k, v in data.items()
+        if v not in ([], "", "", None)
+        and k not in ["people_also_viewed", "certifications"]
+    }
+
+    if data.get("groups"):
+        for group_dict in data.get("groups"):
+            group_dict.pop("profile_pic_url")
+
+    return data
+
+
+def get_data(isMock=False):
+    if isMock:
+        response = requests.get(mock_data_endpoint)
+    else:
+        header_dic = {"Authorization": f'Bearer {os.environ.get("PROXYCURL_API_KEY")}'}
+        response = requests.get(
+            "https://nubela.co/proxycurl/api/v2/linkedin",
+            params={"url": linkedin_profile_url},
+            headers=header_dic,
+        )
+
+    return response
