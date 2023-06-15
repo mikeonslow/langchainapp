2d088210e8598c9371d1098825ed01a47a5e53cc
clarityits <clarityits@clarityvoice.com>
2023-06-14 23:07:52 -0400

    Added root route to flask router

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from ice_breaker import ice_break
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 
 @app.route("/process", methods=["POST"])
 def process():
